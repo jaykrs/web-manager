33313b9e48f9b2e0c5281416bc9e2a95fdd730b5
Kumar <jayant.kumar@natwest.com>
2025-11-17 21:28:42 +0530

    deleted

--- src/components/ui/loading/ripleloader.tsx
+++
@@ -1,8 +0,0 @@
-import { Riple } from "react-loading-indicators";
-export const RipleLoader = () => {
-    return (
-        <>
-            <Riple color="#2143BE" size="medium" text="" textColor="" />
-        </>
-    );
-};
